7adeb1033d20343122746ee66eda6826b7f1b458
Karim <20050570+yoshiyes@users.noreply.github.com>
2021-03-22 16:14:55 +0100

    Update build method

--- build/build-debian.sh
+++ build/build-debian.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
-echo -> Build Debian package
+if (( $# != 1 ))
+then
+  echo "Usage: ./build-debian.sh [package_version]"
+  echo "Example: ./build-debian.sh 1.0.0"
+  exit 1
+fi
+
+echo Build Debian package
 
 # Build
+cd build
 dotnet publish ../src/Accessh.Daemon/Accessh.Daemon.csproj -c Release -o ./app -r linux-x64 --self-contained true -p:PublishSingleFile=true -p:PublishReadyToRun=true
 
 # Move app
@@ -14,9 +22,14 @@ mv ./deb/opt/sh-daemon/config.json ./deb/etc/sh-daemon/config.json
 rm -f deb/opt/sh-daemon/.gitkeep
 rm -f deb/etc/sh-daemon/.gitkeep
 
+# Set permission
+chmod 755 -R deb
+
 # Create debian package
 dpkg-deb --build deb
 
 # Finalize
-mv deb.deb shdaemon_1.0-1_amd64.deb
+mv deb.deb shdaemon_$1-1_amd64.deb
 rm -rf app/
+
+echo The Debian package has been created
